1389f9be0207ff0f634ac34cc03a0a2236500f4b
nordrussian <nordrussian@ya.ru>
2021-10-02 22:37:03 +0300

    lesson 2 add homework

--- Lesson_2/homework_2.2.py
+++ Lesson_2/homework_2.2.py
@@ -4,8 +4,16 @@
 
 my_list = input("Введите данные для формирования списка: ")
 
-my_list.append()
+input_my_list = my_list.split()
+
+len_list = len(input_my_list)
+el = 0
+if len_list > 1:
+    while el < len_list - 1:
+        input_my_list[el], input_my_list[el+1] = input_my_list[el+1], input_my_list[el]
+        el += 2
 
 
 
-print(my_list)
+print(input_my_list)
+ 
