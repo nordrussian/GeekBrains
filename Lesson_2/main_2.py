9948b4495cd4aa4910e32e7e999d7a4f8e9b509b
nordrussian <nordrussian@ya.ru>
2021-10-12 03:10:06 +0300

    lesson 5

---
+++ Lesson_2/main_2.py
@@ -0,0 +1,50 @@
+
+
+
+# my_dict = {'one':1, 'two':2, 'three':3}
+# for item in my_dict:
+#     print(my_dict[item])
+
+
+# my_str = "Hello world"
+# for item in my_str:
+#     print(item*2)
+
+
+
+
+# my_list = [1, 2, 3, 4, 5]
+
+# for item in my_list:
+#     if item % 2 == 1:
+#         print(item**256)
+
+# my_dict = {
+#     'Damir':25,
+#     'Anna':18
+# }
+
+# print(my_dict)
+
+# my_dict['Artur'] = 30
+# print(my_dict)
+
+
+# # my_list = [1, 2, 3, 4]
+
+# # my_list.insert(2, 0)
+
+# # print(my_list.index(2))
+
+# # # test_str = 'Hello, world class'
+
+# # # print(len(test_str[:6:2])) #длина строки
+# # # print(test_str.split('.')) #разделение строки
+
+# # # print(test_str.title()) #
+# # # print(test_str.lower()) #
+# # # print(test_str.capitalize()) #
+# # # print(test_str.lower()) #
+
+# # # print(chr(33))
+# # # print(ord('!'))
